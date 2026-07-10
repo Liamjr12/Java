dade0948dff5cb235d3efdade8cfc0a2a2f3984c
Liamjr12 <lampad.isidrojr.s@gmail.com>
2026-07-10 19:00:34 +0800

    Bug fixed

--- src/Fundamentals/CarRentalCalculator.java
+++ src/Fundamentals/CarRentalCalculator.java
@@ -13,87 +13,56 @@ public class CarRentalCalculator {
 
         while (true) {
             try {
-                System.out.print("\nCar type: ");
-                // Fixing bug...
-
-                System.out.print("Days: ");
-                // Fixing bug...
-
-                System.out.print("Insurance: ");
-                // Fixing bug...
-
-                System.out.print("Include driver: ");
-                boolean includeDriver = readBoolean(scanner);
-
-                // Fixing bug...
-                // String line = rentCar(type, days, insurance, includeDriver);
-                // System.out.println("\n===== TRANSACTION =====");
-                // System.out.println(line.concat("\n"));
-
-                System.out.print("Continue(Yes/No)? ");
-                String continueProcess = scanner.nextLine().trim();
-
-                if (continueProcess.equalsIgnoreCase("No")) {
-                    System.out.println("Exiting...");
-                    scanner.close();
-                    System.exit(0);
-                } 
+                String selection = readString(scanner, "\nCar types: ");
+                int days = readInt(scanner, "Days: ");
+                double insurance = readDouble(scanner, "Insurance: ");
+                boolean includeDriver = readBoolean(scanner, "Include driver: ");
+                
+                System.out.println("\n===== TRANSACTION =====");
+                rentCar(selection, days, insurance, includeDriver);
 
             } catch (NumberFormatException e) {
-                System.out.println("Invalid input");
+                System.out.println(e.getMessage());
             }
         }
     }
 
-    /*  Fixing bug....
-    private static String rentCar(String type, int days, double insurance, boolean includeDriver) {
-        double price = carPrice(type);
+    private static void rentCar(String type, int days, double insurance, boolean includeDriver) {
+        double price = getCarType(type);
         double rentalFee = price*days;
         double insuranceFee = rentalFee - insurance;
         double driverFee = (includeDriver = true) ? rentalFee + 500 : rentalFee;
         double GrandTotal = rentalFee + insuranceFee + driverFee;
 
-        return
-        "Rental fee: " + rentalFee +
-        "\nInsurance fee: " + insuranceFee +
-        "\nDriver fee: " + driverFee +
-        "\nGrand total: " + GrandTotal;
+        System.out.println("Rental fee: " + rentalFee);
+        System.out.println("Insurance fee: " + insuranceFee);
+        System.out.println("Driver fee: " + driverFee);
+        System.out.println("Grand total: " + GrandTotal);
     }
 
-    private static double carPrice(String type) {
-        Scanner scanner = new Scanner(System.in);
-        String carTypes = scanner.nextLine().trim().toUpperCase();
+    private static double getCarType(String selection) {
+        CarType types = CarType.valueOf(selection.toUpperCase().trim());
 
-        if (carTypes.equalsIgnoreCase("Exit")) {
-            System.out.println("Exiting...");
-            scanner.close();
-            System.exit(0);
-        }
-
-        try {
-            CarType types = CarType.valueOf(carTypes);
-
-            return switch (types) {
-                case SEDAN -> 5000;
-                case SUV -> 6000;
-                case HATCHBACK -> 7000;
-                case COUPE -> 8000;
-                case CONVERTIBLE -> 9000;
-                case STATION_WAGON -> 10000;
-                case MINIVAN -> 11000;
-                case CROSSOVER -> 12000;
-                case SPORTS_CAR -> 13000;
-            };
-
-        } catch (IllegalArgumentException e) {
-            throw new NumberFormatException();
-        }
-        
+        return switch (types) {
+            case SEDAN -> 5000;
+            case SUV -> 6000;
+            case HATCHBACK -> 7000;
+            case COUPE -> 8000;
+            case CONVERTIBLE -> 9000;
+            case STATION_WAGON -> 10000;
+            case MINIVAN -> 11000;
+            case CROSSOVER -> 12000;
+            case SPORTS_CAR -> 13000;
+        };
     }
-    */
 
-    private static boolean readBoolean(Scanner scanner) {
+    private static boolean readBoolean(Scanner scanner, String prompt) {
+        System.out.print(prompt);
         String input = scanner.nextLine().trim();
+
+        if (input.matches(".*[^a-zA-Z].*")) {
+            throw new IllegalArgumentException("Input only accepts yes and no");
+        }
 
         if (input.equalsIgnoreCase("Exit")) {
             System.out.println("Exiting...");
@@ -106,5 +75,56 @@ public class CarRentalCalculator {
         } 
 
         return false;
+    }
+
+    private static double readDouble(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        String input = scanner.nextLine().trim();
+
+        if (input.equalsIgnoreCase("Exit")) {
+            System.out.println("Exiting...");
+            scanner.close();
+            System.exit(0);
+        }
+
+        if (!input.matches("\\d{1,}*")) {
+            throw new IllegalArgumentException("Input only accepts any real numbers");
+        }
+
+        return Double.parseDouble(input);
+    }
+
+    private static int readInt(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        String input = scanner.nextLine().trim();
+
+        if (input.equalsIgnoreCase("Exit")) {
+            System.out.println("Exiting...");
+            scanner.close();
+            System.exit(0);
+        }
+
+        if (!input.matches("\\d{1,}*")) {
+            throw new IllegalArgumentException("Input only accepts any real numbers");
+        }
+
+        return Integer.parseInt(input);
+    }
+
+    private static String readString(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        String input = scanner.nextLine().trim();
+
+        if (input.equalsIgnoreCase("Exit")) {
+            System.out.println("Exiting...");
+            scanner.close();
+            System.exit(0);
+        }
+
+        if (!input.matches("[a-zA-Z]")) {
+            throw new IllegalArgumentException("Input only accepts letters");
+        }
+
+        return input;
     }
 }
