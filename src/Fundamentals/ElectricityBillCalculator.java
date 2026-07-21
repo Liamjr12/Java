d5a90ade42dc86f5b4f9c7876d7071aee83ad5f0
Liamjr12 <lampad.isidrojr.s@gmail.com>
2026-07-21 15:53:15 +0800

    Improved bill payment

--- src/Fundamentals/ElectricityBillCalculator.java
+++ src/Fundamentals/ElectricityBillCalculator.java
@@ -3,51 +3,113 @@ package Fundamentals;
 import java.util.Scanner;
 
 public class ElectricityBillCalculator {
-    public static void main(String[] args) throws Exception {
-
+    private static int billCount = 0;
+    private static double balance = 0;
+    public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        double[] electricityBill = new double[10];
 
         while (true) {
-            try {
-                double previousReading = readDouble(scanner, "\nPrevious reading: ");
-                double currentReading = readDouble(scanner, "Current reading: ");
-                double ratePerKwh = readDouble(scanner, "Rate per kWh: ");
+            System.out.println("""
+            \n******** ELECTRICITY BILL STATUS ********
+            1. Calculate bill
+            2. Total amount to pay
+            3. Pay electricity bill
+            4. View highest and lowest bill
+            5. Exit""");
 
-                System.out.println("\n===== ELECTRICITY BILL DETAILS =====");
-                getElectricityBill(previousReading, currentReading, ratePerKwh);
-            } catch (Exception e) {
+            try {
+                System.out.print("\nChoice: ");
+                int choice = Integer.parseInt(scanner.nextLine().trim());
+
+                switch (choice) {
+                    case 1 -> insertBill(scanner, electricityBill);
+                    case 2 -> System.out.println("Due: " + balance);
+                    case 3 -> payBill(scanner, electricityBill);
+                    case 4 -> findHighestBill(electricityBill);
+                    case 5 -> {
+                        System.out.println("Terminating program...");
+                        scanner.close();
+                        System.out.println("Program terminated");
+                        System.exit(0);
+                    }
+                }
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
-    }
-
-    private static void getElectricityBill(double previousReading, double currentReading, double ratePerKwh) {
-        double unitsConsumed = currentReading - previousReading;
-        double totalBill = unitsConsumed*ratePerKwh;
-        double lateFee = totalBill*0.02;
-        double finalBill = totalBill+lateFee;
-
-        System.out.println("Units consumed: " + unitsConsumed);
-        System.out.println("Total bill: " + totalBill);
-        System.out.println("Late fee: " + lateFee);
-        System.out.println("Final bill: " + finalBill); 
     }
 
     private static double readDouble(Scanner scanner, String prompt) {
         System.out.print(prompt);
         String input = scanner.nextLine().trim();
 
-        if (input.equalsIgnoreCase("exit")) {
-            System.out.println("Program terminated.");
-            scanner.close();
-            System.exit(0);
+        if (input.equalsIgnoreCase("Return")) {
+            throw new IllegalArgumentException("Returning...");
         }
 
-        if (input.matches("[a-zA-Z]+")) {
+        if (!input.matches("\\d{1,}")) {
             throw new IllegalArgumentException("Input requires number only");
         }
 
-        return Double.parseDouble(input);
+        try {
+            return Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("The input only accepts numbers");
+        }
+    }
+
+    private static double insertBill(Scanner scanner, double[] electricityBill) {
+        double previousReading = readDouble(scanner, "Previous reading: ");
+        double currentReading = readDouble(scanner, "Current reading: ");
+        double ratePerKwh = readDouble(scanner, "Rate per kWh: ");
+
+        if (currentReading <= previousReading) {
+            throw new IllegalArgumentException("Current reading must be greater than the previous reading.");
+        }
+
+        double unitsConsumed = currentReading - previousReading;
+        double totalBill = unitsConsumed*ratePerKwh;
+        double lateFee = totalBill*0.02;
+        double finalBill = totalBill+lateFee;
+
+        System.out.println("\n********* STATUS ********");
+        System.out.println("Units consumed: " + unitsConsumed);
+        System.out.println("Total bill: " + totalBill);
+        System.out.println("Late fee: " + lateFee);
+        System.out.println("Final bill: " + finalBill); 
+
+        balance += finalBill;
+        return electricityBill[billCount++] = finalBill;
+    }
+
+    private static void payBill(Scanner scanner, double[] electricityBill) {
+        double payment = readDouble(scanner, "Amount: ");
+
+        if (payment > balance) {
+            throw new IllegalArgumentException("Payment exceeds remaining balance");
+        }
+
+        balance -= payment;
+        System.out.println("Remaining balance: " + balance);
+    }
+
+    private static void findHighestBill(double[] electricityBill) {
+        double max = electricityBill[0];
+        double min = electricityBill[0];
+
+        for (int i = 0; i < billCount; i++) {
+            if (electricityBill[i] > max) {
+                max = electricityBill[i];
+            }
+            
+            if (electricityBill[i] < min) {
+                min = electricityBill[i];
+            }
+        }
+        
+        System.out.println("Highest bill: " + max);
+        System.out.println("Lowest bill: " + min);
     }
 
 }
--- src/Fundamentals/LoanCalculator.java
+++ src/Fundamentals/LoanCalculator.java
@@ -3,25 +3,69 @@ package Fundamentals;
 import java.util.Scanner;
 
 public class LoanCalculator {
-    public static void main(String[] args) throws Exception {
+    private static int loanCount = 0;
+    private static double balance = 0;
+    public static void main(String[] args) {
 
         final Scanner scanner = new Scanner(System.in);
-
+        double[] loans = new double[10];
+        
         while (true) {
-            try {
-                double loanAmount = readDouble(scanner, "Loan amount: ");
-                double interestRate = readDouble(scanner, "Interest rate: ");
-                double year = readDouble(scanner, "Year: ");
+            System.out.println("""
+            \n******** LOANING SYSTEM ********
+            1. Loan
+            2. Total loan
+            3. Due 
+            4. Pay loan
+            5. Exit""");
 
-                System.out.println("\n===== TRANSACTION =====");
-                getMonthlyPayment(loanAmount, interestRate, year);
+            try {
+                System.out.print("\nChoice: ");
+                int choice = Integer.parseInt(scanner.nextLine().trim());
+
+                switch (choice) {
+                    case 1 -> insertLoan(scanner, loans);
+                    case 2 -> displayLoan(loans);
+                    case 3 -> System.out.println("Due: " + balance);
+                    case 4 -> payLoan(scanner, loans);
+                    case 5 -> {
+                        System.out.println("Terminating program...");
+                        scanner.close();
+                        System.out.println("Program terminated");
+                        System.exit(0);
+                    }
+                }
+
             } catch (RuntimeException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-    private static void getMonthlyPayment(double loanAmount, double interestRate, double year) {
+    private static double readDouble(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        String input = scanner.nextLine().trim();
+
+        if (input.equalsIgnoreCase("Return")) {
+            throw new RuntimeException("Returning...");
+        }
+
+        if (!input.matches("\\d{1,}")) {
+            throw new RuntimeException("The input only accepts numbers");
+        }
+
+        try {
+            return Double.parseDouble(input); 
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("The input only accepts numbers");
+        }
+    }
+
+    private static double insertLoan(Scanner scanner, double[] loans) {
+        double loanAmount = readDouble(scanner, "Loan amount: ");
+        double interestRate = readDouble(scanner, "Interest rate: ");
+        double year = readDouble(scanner, "Year: ");
+
         double rate = (interestRate/100)/12;
         double months = year*12;
 
@@ -32,25 +76,40 @@ public class LoanCalculator {
         double TotalPayment = MonthlyPayment*months;
         double TotalInterest = TotalPayment - loanAmount;
 
+        System.out.println("\n******** LOAN STATUS ********");
         System.out.println("Total interest: " + TotalInterest);
         System.out.println("Total payment: " + TotalPayment);
-        System.out.println("Montly payment:" + MonthlyPayment);
+        System.out.println("Montly payment: " + MonthlyPayment);
+
+        balance += TotalPayment;
+        return loans[loanCount++] = TotalPayment;
     }
 
-    private static double readDouble(Scanner scanner, String prompt) {
-        System.out.print(prompt);
-        String input = scanner.nextLine().trim();
+    private static void displayLoan(double[] loans) {
+        int count = 1;
+        System.out.println("\n******** TOTAL LOANS ********");
+        for (int i = 0; i < loanCount; i++) {
+            System.out.println((count++) + ". Loan: " + loans[i]);
+        }
+        System.out.println("Total: " + totalPayment(loans));
+    }
 
-        if (input.equalsIgnoreCase("Exit")) {
-            System.out.println("Exiting...");
-            scanner.close();
-            System.exit(0);
+    private static double totalPayment (double[] loans) {
+        int sum = 0;
+        for (int i = 0; i < loanCount; i++) {
+            sum += loans[i];
+        }
+        return sum;
+    }
+
+    private static void payLoan(Scanner scanner, double[] loans) {
+        double payment = readDouble(scanner, "Amount: ");
+
+        if (payment > balance) {
+            throw new RuntimeException("Payment exceeds remaining balance");
         }
 
-        if (input.matches("\\d{1,4}")) {
-            throw new IllegalArgumentException("An input must only contain a numbers");
-        }
-
-        return Double.parseDouble(input);
+        balance -= payment;
+        System.out.println("Remaining balance: " + balance);
     }
 }
